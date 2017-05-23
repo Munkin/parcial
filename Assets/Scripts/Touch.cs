e24265991847e45440bff667696a1ebec1165ce8
LAPTOP-V8FNP6IJ\admon <Victormunera1@hotmail.com>
2017-05-23 12:47:14 -0500

    Final

---
+++ Assets/Scripts/Touch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : MonoBehaviour {
+    public float speed;
+    // Use this for initialization
+    void Start () {
+        speed = 0.20f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount == 1) { transform.Translate(Input.touches[0].deltaPosition.x * speed, Input.touches[0].deltaPosition.y * speed, 0); }
+    }
+}
+
+
+
+
